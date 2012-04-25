14737a772aa48889c8e7ae2cc698753ced44db0a
William Hindenburg <Will@Williams-MacBook-Pro.local>
2012-04-24 21:07:33 -0500

    Now under source control

---
+++ ImagoDei/ImagoDeiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ImagoDeiAppDelegate.h
+//  ImagoDei
+//
+//  Created by Will Hindenburg on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagoDeiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImagoDei/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImagoDei
+//
+//  Created by Will Hindenburg on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ImagoDeiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ImagoDeiAppDelegate class]));
+    }
+}
