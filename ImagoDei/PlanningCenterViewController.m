3167092b8239b1a4fc68957ff3b6ee2bbf7c27b5
William Hindenburg <Will@Williams-MacBook-Pro.local>
2012-05-25 11:13:09 -0500

    Added GTMO files to support Planning Center sign in

--- ImagoDei/PlanningCenterViewController.m
+++ ImagoDei/PlanningCenterViewController.m
@@ -7,12 +7,67 @@
 //
 
 #import "PlanningCenterViewController.h"
+#import "GTMOAuthAuthentication.h"
+#import "GTMOAuthViewControllerTouch.h"
+
+#define CONSUMER_KEY @"VmHJumSJqVS80j5TejhH"
+#define CONSUMER_SECRECT @"NkutqYLfideVKX2nqHRV9UIIGYe8rA4qVtu29hu1"
 
 @interface PlanningCenterViewController ()
+@property (nonatomic, strong)GTMOAuthAuthentication *authentication;
 
 @end
 
 @implementation PlanningCenterViewController
+@synthesize authentication = _authentication;
+
+- (GTMOAuthAuthentication *)myCustomAuth {
+    NSString *myConsumerKey = CONSUMER_KEY;    // pre-registered with service
+    NSString *myConsumerSecret = CONSUMER_SECRECT; // pre-assigned by service
+    
+    GTMOAuthAuthentication *auth;
+    auth = [[GTMOAuthAuthentication alloc] initWithSignatureMethod:kGTMOAuthSignatureMethodHMAC_SHA1
+                                                        consumerKey:myConsumerKey
+                                                         privateKey:myConsumerSecret];
+    
+    // setting the service name lets us inspect the auth object later to know
+    // what service it is for
+    auth.serviceProvider = @"Planning Center";
+    
+    return auth;
+}
+
+- (void)signInToCustomService
+{
+    NSURL *requestURL = [NSURL URLWithString:@"https://www.planningcenteronline.com/oauth/request_token"];
+    NSURL *accessURL = [NSURL URLWithString:@"https://www.planningcenteronline.com/oauth/access_token"];
+    NSURL *authorizeURL = [NSURL URLWithString:@"https://www.planningcenteronline.com/oauth/authorize"];
+    NSString *scope = @"https://www.planningcenteronline.com/oauth/";
+    
+    GTMOAuthAuthentication *auth = [self myCustomAuth];
+    
+    // set the callback URL to which the site should redirect, and for which
+    // the OAuth controller should look to determine when sign-in has
+    // finished or been canceled
+    //
+    // This URL does not need to be for an actual web page
+    [auth setCallback:@"http://www.example.com/OAuthCallback"];
+    
+    // Display the autentication view
+    GTMOAuthViewControllerTouch *viewController;
+    viewController = [[GTMOAuthViewControllerTouch alloc] initWithScope:scope
+                                                               language:nil
+                                                        requestTokenURL:requestURL
+                                                      authorizeTokenURL:authorizeURL
+                                                         accessTokenURL:accessURL
+                                                         authentication:auth
+                                                         appServiceName:@"Planning Center: Custom Service"
+                                                               delegate:self
+                                                       finishedSelector:@selector(viewController:finishedWithAuth:error:)];
+    
+    [[self navigationController] pushViewController:viewController
+                                           animated:YES];
+}
 
 - (void)awakeFromNib
 {
@@ -21,6 +76,44 @@
     [self.tabBarItem setFinishedSelectedImage:[UIImage imageNamed:@"pco-logo-active.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"pco-logo-inactive.png"]];
     self.tabBarItem.title = @"Planning Center";
     [self.activityIndicator stopAnimating];
+    [GTMOAuthViewControllerTouch removeParamsFromKeychainForName:@"Planning Center: Custom Service"];
+    
+
+}
+
+- (void)viewDidLoad
+{
+    // Get the saved authentication, if any, from the keychain.
+    
+    //self.authentication = [self myCustomAuth];
+    [self signInToCustomService];
+    
+    /*if (self.authentication) 
+    {
+        BOOL didAuth = [GTMOAuthViewControllerTouch authorizeFromKeychainForName:@"Planning Center: Custom Service"
+                                                                  authentication:self.authentication];
+        if (didAuth)
+        {
+            NSLog(@"Data Saved");
+        }
+    }
+    
+    // retain the authentication object, which holds the auth tokens
+    //
+    // we can determine later if the auth object contains an access token
+    // by calling its -canAuthorize method
+    //[self setAuthentication:auth];*/
+}
+
+- (void)viewController:(GTMOAuthViewControllerTouch *)viewController
+      finishedWithAuth:(GTMOAuthAuthentication *)auth
+                 error:(NSError *)error 
+{
+    if (error != nil) {
+        NSLog(@"Error Signing In");
+    } else {
+        NSLog(@"Signed In");
+    }
 }
 
 @end
